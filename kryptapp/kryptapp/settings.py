23075e0a3cc26d7fef7308a5c1d279f9bb38a6d8
root <root@ip-172-31-36-224.us-west-2.compute.internal>
2017-02-06 00:48:46 +0000

    final initial release and set up

--- kryptapp/kryptapp/settings.py
+++ kryptapp/kryptapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+7!te&ume*il16_2npfxng#(bz6o@5nf%d#3@etig1o&mbz!!w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kryptchat.me']
 
 
 # Application definition
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'kryptapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kryptchat',
+        'USER': 'kryptchat',
+        'PASSWORD': 'nljq}:b;',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
